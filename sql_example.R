d2d717a64b4a6cba3b0741e0beca533ffd4d27f0
Julien Bodelet <julien.bodelet@jacobscenter.uzh.ch>
2024-01-23 15:29:10 +0100

    Update sql_example.R

--- sql_example.R
+++ sql_example.R
@@ -19,12 +19,11 @@ plot(sql)
 # q= 3 factor:
 #==============
 
-q <- 3
-sim <- simulate_afm(n = 150, p = 300, q = q)
+q <- 2
+sim <- simulate_afm(n = 150, p = 200, q = q)
 
-sql <- SQL(sim$data, q = q)
+sql <- SQL(sim$data, q = q, tol = 1e-05, d= 6)
 sql
 abs( cor(sim$factor, sql$factor) )
-
 
 
